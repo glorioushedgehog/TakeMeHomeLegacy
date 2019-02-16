694ad7c3bc3795e4055f2a9e12f09826327d403e
glorioushedgehog <pdevlin@udallas.edu>
2019-02-16 15:50:58 -0600

    add static to urls

--- tmh/urls.py
+++ tmh/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('fr', views.facial_recognition, name='fr'),
     path('', views.index, name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
