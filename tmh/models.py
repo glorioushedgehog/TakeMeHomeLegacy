9ec062d0106415038c88f0923b99b4fc16c1ed7f
glorioushedgehog <pdevlin@udallas.edu>
2019-03-22 16:00:57 -0500

    fix static file directories

--- tmh/models.py
+++ tmh/models.py
@@ -123,7 +123,7 @@ class Person(models.Model):
 
 class ImageData(models.Model):
     primarykey = models.CharField(db_column='PrimaryKey', primary_key=True, max_length=22)
-    picture = models.ImageField(db_column='PictureURL', upload_to='static/')
+    picture = models.ImageField(db_column='PictureURL', upload_to='tmh/static/images')
     embedding = models.TextField(db_column='Embedding', blank=True, null=True)
 
     class Meta:
