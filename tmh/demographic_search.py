57a1f75d2b9fd8566f13324753d91627e6cf60a5
glorioushedgehog <pdevlin@udallas.edu>
2019-05-07 18:57:20 -0500

    clean demographic search

--- tmh/demographic_search.py
+++ tmh/demographic_search.py
@@ -3,7 +3,7 @@ from tmh.models import Person
 AGE_GUESS_MARGIN = 5
 
 
-def weight_match(a_person, input_weight, attached_weight):
+def weight_match(input_weight, attached_weight):
     """
     checks if a given weight is within the acceptable threshold
     :param input_weight: the weight of the search target
@@ -14,7 +14,7 @@ def weight_match(a_person, input_weight, attached_weight):
     return attached_weight - WEIGHT_THRESH <= input_weight <= attached_weight + WEIGHT_THRESH
 
 
-def height_match(a_person, input_height, attached_height):
+def height_match(input_height, attached_height):
     """
     checks if a given height is within the acceptable threshold
     :param input_height: the height of the search target
@@ -25,7 +25,7 @@ def height_match(a_person, input_height, attached_height):
     return attached_height - HEIGHT_THRESH <= input_height <= attached_height + HEIGHT_THRESH
 
 
-def dob_match(a_person, input_dob, attached_dob):
+def dob_match(input_dob, attached_dob):
     """
     returns true if some fields match, false otherwise
     :param input_dob: the search dob
@@ -42,7 +42,7 @@ def dob_match(a_person, input_dob, attached_dob):
     return True
 
 
-def zip_match(a_person, input_zip, attached_zip):
+def zip_match(input_zip, attached_zip):
     """
     checks if two zips are within a reasonable distance of one another
     :param input_zip: the zip given as a search term
@@ -52,7 +52,11 @@ def zip_match(a_person, input_zip, attached_zip):
     return input_zip == attached_zip
 
 
-def string_alignment(a_person, str_one, str_two):
+def dob_year_match(input_dob_year, attached_dob_year):
+    return attached_dob_year - AGE_GUESS_MARGIN <= input_dob_year <= attached_dob_year + AGE_GUESS_MARGIN
+
+
+def string_alignment(str_one, str_two):
     """
     Checks alignment between two strings
     :param str_one: first string (the passed in search term)
@@ -98,90 +102,97 @@ def string_alignment(a_person, str_one, str_two):
     return False
 
 
-def search(a_person, first_name="", last_name="", middle_name="", name_to_call_me="", home_city="", home_state="",
-           home_zip="", dob="", dob_year="", hair="", eyes="", race="", sex="", height="", weight="", ):
+def search(a_person, search_params):
     """
-    returns a boolean based on if there is a match between the parameters and the instance's details
-    :param first_name: the first name of the search target
-    :param last_name: the last name of the search target
-    :param middle_name: the middle name of the search target
-    :param name_to_call_me: the nickname of the search target
-    :param home_city: the home city of the search target
-    :param home_state: the home state of the search target
-    :param home_zip: the home zip of the search target
-    :param dob: the date of birth of the search target
-    :param dob_year: the date of birth year of the search target
-    :param hair: the hair color of the search target
-    :param eyes: the eye color of the search target
-    :param race: the race/ethnicity of the search target
-    :param sex: the sex of the search target
-    :param height: the approximate height of the search target
-    :param weight: the weight of the search target
-    :return: True if over 75% of the search terms match (to all given paramas), false otherwise
+    tells if there is a match between the parameters and a person
+    :param a_person: the person being checked
+    :param search_params: the values the user entered into demographic search
+    :return: True if over 75% of the search terms match (to all given params), false otherwise
     """
+
     total = 0
     correct = 0
-    if first_name != "":
-        if string_alignment(a_person, first_name, a_person.first_name):
-            correct += 1
-            print("first_name correct")
-        total += 1
-    if last_name != "":
-        if string_alignment(a_person, last_name, a_person.last_name):
+
+    if search_params['dob_year'] is not None:
+        if dob_year_match(search_params['dob_year'], a_person.dob_year):
             correct += 1
         total += 1
-    if middle_name != "":
-        if string_alignment(a_person, middle_name, a_person.middle_name):
+    if search_params['height'] is not None:
+        if height_match(search_params['height'], a_person.height):
             correct += 1
         total += 1
-    if name_to_call_me != "":
-        if string_alignment(a_person, name_to_call_me, a_person.name_to_call_me):
+    if search_params['weight'] is not None:
+        if weight_match(search_params['weight'], a_person.weight):
             correct += 1
         total += 1
-    if home_city != "":
-        if home_city.strip() == a_person.home_city:
+    if search_params['sex'] is not None:
+        if search_params['sex'] == a_person.sex:
             correct += 1
         total += 1
-    if home_state != "":
-        if home_state == a_person.home_state:
+    if search_params['hair'] is not None:
+        if search_params['hair'] == a_person.hair:
             correct += 1
         total += 1
-    if home_zip != "":
-        if zip_match(a_person, home_zip, a_person.home_zip):
+    if search_params['eyes'] is not None:
+        if search_params['eyes'] == a_person.eyes:
             correct += 1
         total += 1
-    if dob != "":
-        if dob_match(a_person, dob, a_person.dob):
+    if search_params['race'] is not None:
+        if search_params['race'] == a_person.race:
             correct += 1
         total += 1
-    if dob_year != "":
-        if a_person.dob_year - AGE_GUESS_MARGIN <= dob_year <= a_person.dob_year + AGE_GUESS_MARGIN:
+
+    if search_params['last_name'] is not None:
+        if string_alignment(search_params['last_name'], a_person.last_name):
             correct += 1
         total += 1
-    if hair != "":
-        if hair == a_person.hair:
+    if search_params['first_name'] is not None:
+        if string_alignment(search_params['first_name'], a_person.first_name):
             correct += 1
         total += 1
-    if eyes != "":
-        if eyes == a_person.eyes:
+    if search_params['middle_name'] is not None:
+        if string_alignment(search_params['middle_name'], a_person.middle_name):
             correct += 1
         total += 1
-    if race != "":
-        if race == a_person.race:
+    if search_params['name_to_call_me'] is not None:
+        if string_alignment(search_params['name_to_call_me'], a_person.name_to_call_me):
             correct += 1
         total += 1
-    if sex != "":
-        if sex == a_person.sex:
+
+    if search_params['braclet_id'] is not None:
+        if string_alignment(search_params['braclet_id'], a_person.braclet_id):
             correct += 1
         total += 1
-    if height != "":
-        if height_match(a_person, height, a_person.height):
+    if search_params['record_type'] is not None:
+        if search_params['record_type'] == a_person.record_type:
             correct += 1
         total += 1
-    if weight != "":
-        if weight_match(a_person, weight, a_person.weight):
+    if search_params['organization'] is not None:
+        if search_params['organization'] == a_person.organization:
             correct += 1
         total += 1
+
+    if search_params['home_address'] is not None:
+        if string_alignment(search_params['home_address'], a_person.home_address):
+            correct += 1
+        total += 1
+    if search_params['home_city'] is not None:
+        if string_alignment(search_params['home_city'], a_person.home_city):
+            correct += 1
+        total += 1
+    if search_params['home_state'] is not None:
+        if search_params['home_state'] == a_person.home_state:
+            correct += 1
+        total += 1
+    if search_params['home_zip'] is not None:
+        if zip_match(search_params['home_zip'], a_person.home_zip):
+            correct += 1
+        total += 1
+    if search_params['home_phone'] is not None:
+        if string_alignment(search_params['home_phone'], a_person.home_phone):
+            correct += 1
+        total += 1
+
     if total == 0:
         return False
     print(a_person.first_name, a_person.last_name, correct / total)
@@ -191,127 +202,10 @@ def search(a_person, first_name="", last_name="", middle_name="", name_to_call_m
         return False
 
 
-def get_matching_persons_old(search_params):
-    people = Person.objects.all()
-    first_name = ""
-    last_name = ""
-    middle_name = ""
-    name_to_call_me = ""
-    home_city = ""
-    home_state = ""
-    home_zip = ""
-    dob = ""
-    dob_year = ""
-    hair = ""
-    eyes = ""
-    race = ""
-    sex = ""
-    height = ""
-    weight = ""
-    remove_list = []
-    for entry in search_params:
-        if search_params[entry] is None:
-            remove_list.append(entry)
-    for entry in remove_list:
-        search_params.pop(entry, None)
-    if "first_name" in search_params:
-        first_name = search_params["first_name"]
-    if "last_name" in search_params:
-        last_name = search_params["last_name"]
-    if "middle_name" in search_params:
-        middle_name = search_params["middle_name"]
-    if "name_to_call_me" in search_params:
-        name_to_call_me = search_params["name_to_call_me"]
-    if "home_city" in search_params:
-        home_city = search_params["home_city"]
-    if "home_state" in search_params:
-        home_state = search_params["home_state"]
-    if "home_zip" in search_params:
-        home_zip = search_params["home_zip"]
-    if "dob" in search_params:
-        dob = search_params["dob"]
-    if "dob_year" in search_params:
-        dob_year = search_params["dob_year"]
-    if "hair" in search_params:
-        hair = search_params["hair"]
-    if "eyes" in search_params:
-        eyes = search_params["eyes"]
-    if "race" in search_params:
-        race = search_params["race"]
-    if "sex" in search_params:
-        sex = search_params["sex"]
-    if "height" in search_params:
-        height = search_params["height"]
-    if "weight" in search_params:
-        weight = search_params["weight"]
-
-    persons = []
-    for peep in people:
-        if search(peep, first_name, last_name, middle_name, name_to_call_me, home_city,
-                  home_state, home_zip, dob, dob_year, hair, eyes, race, sex, height,
-                  weight):
-            persons.append(peep)
-    return persons
-
-
 def get_matching_persons(search_params):
     people = Person.objects.all()
-    first_name = ""
-    last_name = ""
-    middle_name = ""
-    name_to_call_me = ""
-    home_city = ""
-    home_state = ""
-    home_zip = ""
-    dob = ""
-    dob_year = ""
-    hair = ""
-    eyes = ""
-    race = ""
-    sex = ""
-    height = ""
-    weight = ""
-    remove_list = []
-    for entry in search_params:
-        if search_params[entry] is None:
-            remove_list.append(entry)
-    for entry in remove_list:
-        search_params.pop(entry, None)
-    if "first_name" in search_params:
-        first_name = search_params["first_name"]
-    if "last_name" in search_params:
-        last_name = search_params["last_name"]
-    if "middle_name" in search_params:
-        middle_name = search_params["middle_name"]
-    if "name_to_call_me" in search_params:
-        name_to_call_me = search_params["name_to_call_me"]
-    if "home_city" in search_params:
-        home_city = search_params["home_city"]
-    if "home_state" in search_params:
-        home_state = search_params["home_state"]
-    if "home_zip" in search_params:
-        home_zip = search_params["home_zip"]
-    if "dob" in search_params:
-        dob = search_params["dob"]
-    if "dob_year" in search_params:
-        dob_year = search_params["dob_year"]
-    if "hair" in search_params:
-        hair = search_params["hair"]
-    if "eyes" in search_params:
-        eyes = search_params["eyes"]
-    if "race" in search_params:
-        race = search_params["race"]
-    if "sex" in search_params:
-        sex = search_params["sex"]
-    if "height" in search_params:
-        height = search_params["height"]
-    if "weight" in search_params:
-        weight = search_params["weight"]
-
     persons = []
-    for peep in people:
-        if search(peep, first_name, last_name, middle_name, name_to_call_me, home_city,
-                  home_state, home_zip, dob, dob_year, hair, eyes, race, sex, height,
-                  weight):
-            persons.append(peep)
+    for a_person in people:
+        if search(a_person, search_params):
+            persons.append(a_person)
     return persons
